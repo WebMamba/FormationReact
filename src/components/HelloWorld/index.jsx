f59a81293e05731c00084e836ae8d8aea0ef8bf8
Matheo Daninos <matheo.daninos@gmail.com>
2021-10-17 12:50:30 +0200

    ajout d'une variable dans le jsx

--- src/components/HelloWorld/index.jsx
+++ src/components/HelloWorld/index.jsx
@@ -1,7 +1,9 @@
 export default function HelloWorld() {
+    const name = "Arthur";
+
     return (
         <>
-            <h1>Bienvenu sur React !</h1>
+            <h1>Bienvenu sur React { name }!</h1>
         </>
     );
 }
