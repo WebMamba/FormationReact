4e889cec3aff207bf2a3c56aab583168cc41dd41
Matheo Daninos <matheo.daninos@gmail.com>
2021-10-17 13:27:03 +0200

    creation de notre première story

---
+++ src/components/HelloWorld/HelloWorld.stories.jsx
@@ -0,0 +1,8 @@
+import HelloWorld from "./index";
+
+export default {
+    title: 'Components/HelloWorld',
+    component: HelloWorld
+}
+
+export const Arthur = () => <HelloWorld/>
--- src/components/HelloWorld/HelloWorld.test.jsx
+++ src/components/HelloWorld/HelloWorld.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import HelloWorld from "../HelloWorld";
+import HelloWorld from ".";
 
 test("render HelloWorld without crashing", () => {
     render(<HelloWorld/>);
