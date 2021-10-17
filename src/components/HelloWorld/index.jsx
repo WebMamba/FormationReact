d3c54f641784c80fa90d6412e14782403a24dbf4
Matheo Daninos <matheo.daninos@gmail.com>
2021-10-17 12:58:22 +0200

    petite parenthèse sur la sécurité

--- src/components/HelloWorld/index.jsx
+++ src/components/HelloWorld/index.jsx
@@ -1,15 +1,9 @@
 export default function HelloWorld() {
-    const name = "Tom";
-
-    if (name !== "Arthur") {
-        return (
-            <h1>T'es pas le bienvenu gamin !</h1>
-        )
-    }
+    const name = <script>{`console.log("hello world")`}</script>;
 
     return (
         <>
-            <h1>Bienvenu sur React { name }!</h1>
+            <h1>Bienvenu sur React { name } !</h1>
         </>
     );
 }
